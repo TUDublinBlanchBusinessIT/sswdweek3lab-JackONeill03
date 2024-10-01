c59d6ccaff46abd4d654f54f7dfe8bd502c90f2a
JackONeill03 <B00155573@mytudublin.ie>
2024-10-01 15:59:27 +0100

    Nested for Loop

---
+++ nestedForLoop.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<TABLE border='3'>";
+for ($i=1; $i<=20; $i++) {
+    //echo "<TR><TD>2</TD><TD>4</TD>";
+    for ($j=1; $j<=15; $j++){
+        echo "<TD>Row $i, Col $j</TD>";
+    }
+    echo "</TR>";
+}
+ echo "</TABLE>";
+
+
+
+?>
